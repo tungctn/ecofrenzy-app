3504338b832e0e4277f9cfdd763a67c1a30d4259
tungjav <tungchuyenthainguyen@gmail.com>
2023-11-26 22:16:25 +0700

    server AI

--- moderation/main.py
+++ moderation/main.py
@@ -36,3 +36,6 @@ def predict_image():
             'success': False,
             'message': 'Không tìm thấy url',
         })
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
